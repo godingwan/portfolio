8826c671dcb392f43846f15a85e9cd03d2b480f5
Joshua Lee <jsungholee@gmail.com>
2013-07-23 20:42:57 -0400

    flower creation at bottom

--- app/assets/javascripts/bot_canvas.js
+++ app/assets/javascripts/bot_canvas.js
@@ -28,30 +28,30 @@ function drawPetals(startX, startY, firstMidX, firstMidY, endX, endY, lastMidX, 
   context_bot.stroke();
 }
 
-function drawStem(startX, startY, botQuarterX, botQuarterY, midX, midY, topQuarterX, topQuarterY, endX, endY) {
-  context_bot.beginPath();
-  context_bot.moveTo(startX, startY);
-  context_bot.quadraticCurveTo(botQuarterX, botQuarterY, midX, midY);
-  context_bot.quadraticCurveTo(topQuarterX, topQuarterY, endX, endY);
-  context_bot.lineWidth = 5;
-  context_bot.strokeStyle = "green";
-  context_bot.stroke();
+// currently buggy with the createRandomFlower()
+// function drawStem(startX, startY, botQuarterX, botQuarterY, midX, midY, topQuarterX, topQuarterY, endX, endY) {
+//   context_bot.beginPath();
+//   context_bot.moveTo(startX, startY);
+//   context_bot.quadraticCurveTo(botQuarterX, botQuarterY, midX, midY);
+//   context_bot.quadraticCurveTo(topQuarterX, topQuarterY, endX, endY);
+//   context_bot.lineWidth = 5;
+//   context_bot.strokeStyle = "green";
+//   context_bot.stroke();
+// }
+
+function createRandomFlower() {
+  var randX = Math.floor(Math.random() * 1200) + 1;
+  var randY = Math.floor(Math.random() * 400) + 1;
+
+  drawFlowerCenter(randX, randY, 10);
+  drawPetals(randX + 10, randY, randX + 40, randY + 20, randX + 60, randY, randX + 40, randY - 20);
+  drawPetals(randX, randY + 10, randX - 20, randY + 40, randX, randY + 60, randX + 20, randY + 40);
+  drawPetals(randX - 10, randY, randX - 40, randY + 20, randX - 60, randY, randX - 40, randY - 20);
+  drawPetals(randX, randY - 10, randX - 20, randY - 40, randX, randY - 60, randX + 20, randY - 40);
+}
+
+canvas_bot.onclick=function() {
+  createRandomFlower();
 }
 
 setupCanvasBot();
-// flower 1
-drawFlowerCenter(400, 200, 10);
-drawPetals(410, 200, 440, 220, 460, 200, 440, 180);
-drawPetals(400, 210, 380, 240, 400, 260, 420, 240);
-drawPetals(390, 200, 360, 220, 340, 200, 360, 180);
-drawPetals(400, 190, 380, 160, 400, 140, 420, 160);
-drawStem(400, 400, 430, 350, 400, 300, 370, 250, 390, 210);
-// flower 2
-drawFlowerCenter(800, 240, 10);
-drawPetals(810, 240, 840, 260, 860, 240, 840, 220);
-drawPetals(800, 250, 780, 280, 800, 300, 820, 280);
-drawPetals(790, 240, 760, 260, 740, 240, 760, 220);
-drawPetals(800, 230, 780, 200, 800, 180, 820, 200);
-drawStem(800, 400, 770, 360, 800, 320, 830, 270, 810, 250)
-
-
