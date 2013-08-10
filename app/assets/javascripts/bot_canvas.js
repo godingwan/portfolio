03dc70c57172486ca072880248c3b3920ebcde46
Joshua Lee <jsungholee@gmail.com>
2013-08-10 16:36:48 -0400

    more professional looking portfolio

--- app/assets/javascripts/bot_canvas.js
+++ app/assets/javascripts/bot_canvas.js
@@ -1,57 +1,57 @@
-var canvas_bot = document.getElementById('bot_canvas');
-var context_bot = canvas_bot.getContext('2d');
+// var canvas_bot = document.getElementById('bot_canvas');
+// var context_bot = canvas_bot.getContext('2d');
 
-function setupCanvasBot() {
-  canvas_bot.height = 400;
-  canvas_bot.width = 1200;
-}
+// function setupCanvasBot() {
+//   canvas_bot.height = 400;
+//   canvas_bot.width = 1200;
+// }
 
-function drawFlowerCenter(centerX, centerY, radius) {
-  context_bot.beginPath();
-  context_bot.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context_bot.fillStyle = "yellow";
-  context_bot.fill();
-  context_bot.lineWidth = 5;
-  context_bot.strokeStyle = '#000';
-  context_bot.stroke();
-}
-
-function drawPetals(startX, startY, firstMidX, firstMidY, endX, endY, lastMidX, lastMidY) {
-  context_bot.beginPath();
-  context_bot.moveTo(startX, startY);
-  context_bot.quadraticCurveTo(firstMidX, firstMidY, endX, endY);
-  context_bot.quadraticCurveTo(lastMidX, lastMidY, startX, startY);
-  context_bot.lineWidth = 5;
-  context_bot.strokeStyle = '#000';
-  context_bot.fillStyle = "white";
-  context_bot.fill();
-  context_bot.stroke();
-}
-
-// currently buggy with the createRandomFlower()
-// function drawStem(startX, startY, botQuarterX, botQuarterY, midX, midY, topQuarterX, topQuarterY, endX, endY) {
+// function drawFlowerCenter(centerX, centerY, radius) {
 //   context_bot.beginPath();
-//   context_bot.moveTo(startX, startY);
-//   context_bot.quadraticCurveTo(botQuarterX, botQuarterY, midX, midY);
-//   context_bot.quadraticCurveTo(topQuarterX, topQuarterY, endX, endY);
+//   context_bot.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+//   context_bot.fillStyle = "yellow";
+//   context_bot.fill();
 //   context_bot.lineWidth = 5;
-//   context_bot.strokeStyle = "green";
+//   context_bot.strokeStyle = '#000';
 //   context_bot.stroke();
 // }
 
-function createRandomFlower() {
-  var randX = Math.floor(Math.random() * 1200) + 1;
-  var randY = Math.floor(Math.random() * 400) + 1;
+// function drawPetals(startX, startY, firstMidX, firstMidY, endX, endY, lastMidX, lastMidY) {
+//   context_bot.beginPath();
+//   context_bot.moveTo(startX, startY);
+//   context_bot.quadraticCurveTo(firstMidX, firstMidY, endX, endY);
+//   context_bot.quadraticCurveTo(lastMidX, lastMidY, startX, startY);
+//   context_bot.lineWidth = 5;
+//   context_bot.strokeStyle = '#000';
+//   context_bot.fillStyle = "white";
+//   context_bot.fill();
+//   context_bot.stroke();
+// }
 
-  drawFlowerCenter(randX, randY, 10);
-  drawPetals(randX + 10, randY, randX + 40, randY + 20, randX + 60, randY, randX + 40, randY - 20);
-  drawPetals(randX, randY + 10, randX - 20, randY + 40, randX, randY + 60, randX + 20, randY + 40);
-  drawPetals(randX - 10, randY, randX - 40, randY + 20, randX - 60, randY, randX - 40, randY - 20);
-  drawPetals(randX, randY - 10, randX - 20, randY - 40, randX, randY - 60, randX + 20, randY - 40);
-}
+// // currently buggy with the createRandomFlower()
+// // function drawStem(startX, startY, botQuarterX, botQuarterY, midX, midY, topQuarterX, topQuarterY, endX, endY) {
+// //   context_bot.beginPath();
+// //   context_bot.moveTo(startX, startY);
+// //   context_bot.quadraticCurveTo(botQuarterX, botQuarterY, midX, midY);
+// //   context_bot.quadraticCurveTo(topQuarterX, topQuarterY, endX, endY);
+// //   context_bot.lineWidth = 5;
+// //   context_bot.strokeStyle = "green";
+// //   context_bot.stroke();
+// // }
 
-canvas_bot.onclick=function() {
-  createRandomFlower();
-}
+// function createRandomFlower() {
+//   var randX = Math.floor(Math.random() * 1200) + 1;
+//   var randY = Math.floor(Math.random() * 400) + 1;
 
-setupCanvasBot();
+//   drawFlowerCenter(randX, randY, 10);
+//   drawPetals(randX + 10, randY, randX + 40, randY + 20, randX + 60, randY, randX + 40, randY - 20);
+//   drawPetals(randX, randY + 10, randX - 20, randY + 40, randX, randY + 60, randX + 20, randY + 40);
+//   drawPetals(randX - 10, randY, randX - 40, randY + 20, randX - 60, randY, randX - 40, randY - 20);
+//   drawPetals(randX, randY - 10, randX - 20, randY - 40, randX, randY - 60, randX + 20, randY - 40);
+// }
+
+// canvas_bot.onclick=function() {
+//   createRandomFlower();
+// }
+
+// setupCanvasBot();
--- app/assets/javascripts/top_canvas.js
+++ app/assets/javascripts/top_canvas.js
@@ -1,192 +1,192 @@
-var canvas_top = document.getElementById('top_canvas');
-var context_top = canvas_top.getContext('2d');
-// canvas drawing
-var mousedown = false;
-var oldx = null;
-var oldy = null;
+// var canvas_top = document.getElementById('top_canvas');
+// var context_top = canvas_top.getContext('2d');
+// // canvas drawing
+// var mousedown = false;
+// var oldx = null;
+// var oldy = null;
 
-function setupCanvasTop() {
-  canvas_top.height = 1200;
-  canvas_top.width = 1200;
-}
+// function setupCanvasTop() {
+//   canvas_top.height = 1200;
+//   canvas_top.width = 1200;
+// }
 
-function drawSun() {
-  var centerX = 20;
-  var centerY = 20;
-  var radius = 100;
+// function drawSun() {
+//   var centerX = 20;
+//   var centerY = 20;
+//   var radius = 100;
 
-  context_top.beginPath();
-  context_top.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context_top.fillStyle = "yellow";
-  context_top.fill();
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "orange";
-  context_top.stroke();
-}
+//   context_top.beginPath();
+//   context_top.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+//   context_top.fillStyle = "yellow";
+//   context_top.fill();
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "orange";
+//   context_top.stroke();
+// }
 
-function drawSunLights() {
-  // yellow lines
-  context_top.beginPath();
-  context_top.moveTo(150, 20);
-  context_top.lineTo(250, 20);
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "yellow";
-  context_top.lineCap = 'round';
-  context_top.stroke();
+// function drawSunLights() {
+//   // yellow lines
+//   context_top.beginPath();
+//   context_top.moveTo(150, 20);
+//   context_top.lineTo(250, 20);
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "yellow";
+//   context_top.lineCap = 'round';
+//   context_top.stroke();
 
-  context_top.beginPath();
-  context_top.moveTo(130, 130);
-  context_top.lineTo(200, 200);
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "yellow";
-  context_top.lineCap = 'round';
-  context_top.stroke();
+//   context_top.beginPath();
+//   context_top.moveTo(130, 130);
+//   context_top.lineTo(200, 200);
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "yellow";
+//   context_top.lineCap = 'round';
+//   context_top.stroke();
 
-  context_top.beginPath();
-  context_top.moveTo(20, 150);
-  context_top.lineTo(20, 250);
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "yellow";
-  context_top.lineCap = 'round';
-  context_top.stroke();
+//   context_top.beginPath();
+//   context_top.moveTo(20, 150);
+//   context_top.lineTo(20, 250);
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "yellow";
+//   context_top.lineCap = 'round';
+//   context_top.stroke();
 
-  // orange lines
-  context_top.beginPath();
-  context_top.moveTo(140, 70);
-  context_top.lineTo(225, 90);
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "orange";
-  context_top.lineCap = 'round';
-  context_top.stroke();
+//   // orange lines
+//   context_top.beginPath();
+//   context_top.moveTo(140, 70);
+//   context_top.lineTo(225, 90);
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "orange";
+//   context_top.lineCap = 'round';
+//   context_top.stroke();
 
-  context_top.beginPath();
-  context_top.moveTo(70, 140);
-  context_top.lineTo(110, 225);
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = "orange";
-  context_top.lineCap = 'round';
-  context_top.stroke();
-}
+//   context_top.beginPath();
+//   context_top.moveTo(70, 140);
+//   context_top.lineTo(110, 225);
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = "orange";
+//   context_top.lineCap = 'round';
+//   context_top.stroke();
+// }
 
-function drawClouds() {
-  // cloud one
-  // begin custom shape
-  context_top.beginPath();
-  context_top.moveTo(270, 180);
-  context_top.bezierCurveTo(230, 200, 230, 250, 330, 250);
-  context_top.bezierCurveTo(350, 280, 420, 280, 440, 250);
-  context_top.bezierCurveTo(520, 250, 520, 220, 490, 200);
-  context_top.bezierCurveTo(530, 140, 470, 130, 440, 150);
-  context_top.bezierCurveTo(420, 105, 350, 120, 350, 150);
-  context_top.bezierCurveTo(300, 105, 250, 120, 270, 180);
-  // complete custom shape
-  context_top.closePath();
-  context_top.fillStyle = "white";
-  context_top.shadowColor = '#999';
-  context_top.shadowBlur = 10;
-  context_top.shadowOffsetX = 5;
-  context_top.shadowOffsetY = 5;
-  context_top.fill();
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = 'rgb(0, 0, 50)';
+// function drawClouds() {
+//   // cloud one
+//   // begin custom shape
+//   context_top.beginPath();
+//   context_top.moveTo(270, 180);
+//   context_top.bezierCurveTo(230, 200, 230, 250, 330, 250);
+//   context_top.bezierCurveTo(350, 280, 420, 280, 440, 250);
+//   context_top.bezierCurveTo(520, 250, 520, 220, 490, 200);
+//   context_top.bezierCurveTo(530, 140, 470, 130, 440, 150);
+//   context_top.bezierCurveTo(420, 105, 350, 120, 350, 150);
+//   context_top.bezierCurveTo(300, 105, 250, 120, 270, 180);
+//   // complete custom shape
+//   context_top.closePath();
+//   context_top.fillStyle = "white";
+//   context_top.shadowColor = '#999';
+//   context_top.shadowBlur = 10;
+//   context_top.shadowOffsetX = 5;
+//   context_top.shadowOffsetY = 5;
+//   context_top.fill();
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = 'rgb(0, 0, 50)';
 
-  id: "cloud_1";
+//   id: "cloud_1";
 
-  context_top.stroke();
+//   context_top.stroke();
 
-  // cloud two
-  context_top.beginPath();
-  context_top.moveTo(770, 80);
-  context_top.bezierCurveTo(730, 100, 730, 150, 830, 150);
-  context_top.bezierCurveTo(850, 180, 920, 180, 940, 150);
-  context_top.bezierCurveTo(1020, 150, 1020, 120, 990, 100);
-  context_top.bezierCurveTo(1030, 40, 970, 30, 940, 50);
-  context_top.bezierCurveTo(920, 05, 850, 20, 850, 50);
-  context_top.bezierCurveTo(800, 05, 750, 20, 770, 80);
-  // complete custom shape
-  context_top.closePath();
-  context_top.fillStyle = "white";
-  context_top.shadowColor = '#999';
-  context_top.shadowBlur = 10;
-  context_top.shadowOffsetX = 7;
-  context_top.shadowOffsetY = 2;
-  context_top.fill();
-  context_top.lineWidth = 5;
-  context_top.strokeStyle = 'rgb(0, 0, 50)';
-  context_top.stroke();
-}
+//   // cloud two
+//   context_top.beginPath();
+//   context_top.moveTo(770, 80);
+//   context_top.bezierCurveTo(730, 100, 730, 150, 830, 150);
+//   context_top.bezierCurveTo(850, 180, 920, 180, 940, 150);
+//   context_top.bezierCurveTo(1020, 150, 1020, 120, 990, 100);
+//   context_top.bezierCurveTo(1030, 40, 970, 30, 940, 50);
+//   context_top.bezierCurveTo(920, 05, 850, 20, 850, 50);
+//   context_top.bezierCurveTo(800, 05, 750, 20, 770, 80);
+//   // complete custom shape
+//   context_top.closePath();
+//   context_top.fillStyle = "white";
+//   context_top.shadowColor = '#999';
+//   context_top.shadowBlur = 10;
+//   context_top.shadowOffsetX = 7;
+//   context_top.shadowOffsetY = 2;
+//   context_top.fill();
+//   context_top.lineWidth = 5;
+//   context_top.strokeStyle = 'rgb(0, 0, 50)';
+//   context_top.stroke();
+// }
 
-function drawMessage() {
-  context_top.font = 'bold 40px serif';
-  context_top.fillStyle = "black";
-  context_top.shadowColor = 'clear';
-  context_top.fillText("Hello, my name is Joshua Lee", 350, 340);
-  context_top.fillText("A web developer", 460, 390);
-  context_top.font = 'normal 30px serif';
-  context_top.fillText("(Draw me a beard)", 500, 430);
-}
+// function drawMessage() {
+//   context_top.font = 'bold 40px serif';
+//   context_top.fillStyle = "black";
+//   context_top.shadowColor = 'clear';
+//   context_top.fillText("Hello, my name is Joshua Lee", 350, 340);
+//   context_top.fillText("A web developer", 460, 390);
+//   context_top.font = 'normal 30px serif';
+//   context_top.fillText("(Draw me a beard)", 500, 430);
+// }
 
-function insertPicture() {
-  var imageObj = new Image();
+// function insertPicture() {
+//   var imageObj = new Image();
 
-  imageObj.onload = function() {
-    context_top.drawImage(imageObj, 200, 460);
-  };
-  imageObj.src = "http://hostmypicture.com/images/picturzmz.jpg";
-}
+//   imageObj.onload = function() {
+//     context_top.drawImage(imageObj, 200, 460);
+//   };
+//   imageObj.src = "http://hostmypicture.com/images/picturzmz.jpg";
+// }
 
-// canvas drawing
-function getMousePos(canvas_top, evt) {
-  var rect = canvas_top.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-}
+// // canvas drawing
+// function getMousePos(canvas_top, evt) {
+//   var rect = canvas_top.getBoundingClientRect();
+//   return {
+//     x: evt.clientX - rect.left,
+//     y: evt.clientY - rect.top
+//   };
+// }
 
-function onmousedown(ev) {
-  var mousePos = getMousePos(canvas_top, ev);
-  mousedown = true;
-  oldx = mousePos.x;
-  oldy = mousePos.y;
-  ev.preventDefault();
-}
+// function onmousedown(ev) {
+//   var mousePos = getMousePos(canvas_top, ev);
+//   mousedown = true;
+//   oldx = mousePos.x;
+//   oldy = mousePos.y;
+//   ev.preventDefault();
+// }
 
-function onmouseup(ev) {
-  mousedown = false;
-  ev.preventDefault();
-}
+// function onmouseup(ev) {
+//   mousedown = false;
+//   ev.preventDefault();
+// }
 
-function onmousemove(ev) {
-  var mousePos = getMousePos(canvas_top, ev);
-  var x = mousePos.x;
-  var y = mousePos.y;
-  if (mousedown) {
-    paint(x, y);
-  }
-}
+// function onmousemove(ev) {
+//   var mousePos = getMousePos(canvas_top, ev);
+//   var x = mousePos.x;
+//   var y = mousePos.y;
+//   if (mousedown) {
+//     paint(x, y);
+//   }
+// }
 
-function paint(x, y) {
-  if (oldx > 0 && oldy > 0) {
-    context_top.moveTo(oldx, oldy);
-  }
-  context_top.lineTo(x, y);
-  context_top.shadowColor = 'clear';
-  context_top.lineWidth = 5;
-  context_top.lineCap = 'round';
-  context_top.strokeStyle = 'rgb(0, 0, 50)';
-  context_top.stroke();
-  oldx = x;
-  oldy = y;
-}
+// function paint(x, y) {
+//   if (oldx > 0 && oldy > 0) {
+//     context_top.moveTo(oldx, oldy);
+//   }
+//   context_top.lineTo(x, y);
+//   context_top.shadowColor = 'clear';
+//   context_top.lineWidth = 5;
+//   context_top.lineCap = 'round';
+//   context_top.strokeStyle = 'rgb(0, 0, 50)';
+//   context_top.stroke();
+//   oldx = x;
+//   oldy = y;
+// }
 
-canvas_top.addEventListener('mousedown', onmousedown, false);
-canvas_top.addEventListener('mouseup', onmouseup, false);
-canvas_top.addEventListener('mousemove', onmousemove, false);
+// canvas_top.addEventListener('mousedown', onmousedown, false);
+// canvas_top.addEventListener('mouseup', onmouseup, false);
+// canvas_top.addEventListener('mousemove', onmousemove, false);
 
-setupCanvasTop();
-drawSun();
-drawSunLights();
-drawClouds();
-drawMessage();
-insertPicture();
+// setupCanvasTop();
+// drawSun();
+// drawSunLights();
+// drawClouds();
+// drawMessage();
+// insertPicture();
